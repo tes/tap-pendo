5727e4049f0ddc9938b71b50a4f72c6a5de77077
Fingal <fingal.plumpton@tes.com>
2021-10-27 15:43:15 +0100

    Support lookback_window being an integer
    Meltano coerces any value to integer for us

--- tap_pendo/streams.py
+++ tap_pendo/streams.py
@@ -468,6 +468,8 @@ class Stream():
 
     def lookback_window(self):
         lookback_window = self.config.get('lookback_window') or '0'
+        if type(lookback_window) == int:
+            return lookback_window
         if not lookback_window.isdigit():
             raise TypeError("lookback_window '{}' is not numeric. Check your configuration".format(lookback_window))
         return int(lookback_window)
